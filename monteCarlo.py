8d302fc46f55288470bd0ba5a3c60128b7989feb
Bartek <kowalczuk.bartek@gmail.com>
2019-01-16 15:17:00 +0100

    Add mean analysis

--- monteCarlo.py
+++ monteCarlo.py
@@ -3,13 +3,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0, nbFlights = 10000, maxOverbooking = 16):
+def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0,
+                nbFlights = 10000, maxOverbooking = 16):
 
     def show_up(probability_show):
         if random.random() <= probability_show:
             return True
-        else: 
-            return False 
+        else:
+            return False
 
     def simulate_flight(nb_tickets_sold, probability_show):
         n = 0 # number of folks who bought tix that will show up
@@ -18,8 +19,8 @@ def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0, 
                 n = n + 1
         return n
 
-    def simulate_net_revenue(nb_tickets_sold, nb_seats, 
-                            probability_show, revenue_per_seat, voucher_cost):
+    def simulate_net_revenue(nb_tickets_sold, nb_seats,
+                             probability_show, revenue_per_seat, voucher_cost):
         # how many ticket purchasers actually showed up?
         nb_shows = simulate_flight(nb_tickets_sold, probability_show)
         # no one bumped from flight if less or equal folks show up than for
@@ -43,10 +44,14 @@ def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0, 
     for tix_overbooked in range(0, max_overbooking):
         nb_tickets_sold = nb_seats + tix_overbooked
         for f in range(0, nb_flights): # simulate nb_flights flights
-            revenue[f][tix_overbooked] = simulate_net_revenue(nb_tickets_sold, nb_seats, probability_show, revenue_per_seat, voucher_cost)
-        
+            revenue[f][tix_overbooked] = simulate_net_revenue(nb_tickets_sold,
+                                                              nb_seats,
+                                                              probability_show,
+                                                              revenue_per_seat,
+                                                              voucher_cost)
+
     # print(revenue)
-    
+
     # f, ax = plt.subplots()
     # l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     # ax.boxplot(revenue)
@@ -54,14 +59,19 @@ def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0, 
     # plt.show()
 
 
-for i in range(350, 500):
-    monteCarlo(revenuePerSeat = i)
+# for i in range(350, 500):
+#     monteCarlo(revenuePerSeat = i)
 
 
-col = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-index = range(10000)
-df = pd.DataFrame(data=revenue[0:,0:], index=index, columns=col)
-df.head(5)
-df.tail(5)
+    col = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    index = range(10000)
+    df = pd.DataFrame(data=revenue[0:,0:], index=index, columns=col)
 
-df.describe()
+    maxValue = max(df.mean())
+    dictionary = dict(df.mean())
+
+    for keys, values in dictionary.items():
+        if (values == maxValue):
+            print(keys)
+
+monteCarlo()
