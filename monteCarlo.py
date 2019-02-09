e90f2135489a2c004ba094e868287a9a09b1e7cb
Bartek <kowalczuk.bartek@gmail.com>
2019-02-09 17:42:28 +0100

    Add probability distinction / voucher levels

--- monteCarlo.py
+++ monteCarlo.py
@@ -3,26 +3,31 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0,
-                nbFlights = 10000, maxOverbooking = 16, interDomestic = 0):
+def monteCarlo (probabilityShowDomestic = 0.9, probabilityShowInter = 0.9375,
+                nbSeats = 100, revenuePerSeat = 450.0, nbFlights = 10000,
+                maxOverbooking = 16, probabilityDomestic = 6/7, isEU = True,
+                isSmallFlight = True):
 
-    def show_up(probabilityShow):
-        if random.random() <= probabilityShow:
-            return True
+    def isDomestic(probabilityDomestic):
+        return random.random() <= probabilityDomestic
+
+    def show_up(probabilityShowDomestic, probabilityShowInter):
+        if isDomestic(probabilityDomestic):
+            return random.random() <= probabilityShowDomestic
         else:
-            return False
+            return random.random() <= probabilityShowInter
 
-    def simulate_flight(nb_tickets_sold, probabilityShow):
-        n = 0 # number of people who bought tix that will show up
+    def simulate_flight(nb_tickets_sold, probabilityShowDomestic, probabilityShowInter):
+        n = 0 # number of people who bought ticket that will show up
         for i in range(0,nb_tickets_sold):
-            if show_up(probabilityShow):
+            if show_up(probabilityShowDomestic, probabilityShowInter):
                 n = n + 1
         return n
 
     def simulate_net_revenue(nb_tickets_sold, nbSeats,
-                             probabilityShow, revenuePerSeat, voucher_cost):
+                             probabilityShowDomestic, probabilityShowInter, revenuePerSeat, voucherCost):
         # how many ticket purchasers actually showed up?
-        nb_shows = simulate_flight(nb_tickets_sold, probabilityShow)
+        nb_shows = simulate_flight(nb_tickets_sold, probabilityShowDomestic, probabilityShowInter)
         # no one bumped from flight if less or equal people show up than for
         #   the number of seats we have
         if nb_shows <= nbSeats:
@@ -30,10 +35,18 @@ def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0,
         # if more customers show up than seats we have, must pay out vouchers
         else:
             vouchers_out = nb_shows - nbSeats
-            return nbSeats * revenuePerSeat - voucher_cost * vouchers_out
+            return nbSeats * revenuePerSeat - voucherCost * vouchers_out
 
-
-    voucher_cost = revenuePerSeat * 2.0 # USD
+    if isEU:
+        if isSmallFlight:
+            voucherCost = 453.42 # USD
+        else:
+            voucherCost = 680.13 # USD
+    else:
+        if isSmallFlight:
+            voucherCost = min(revenuePerSeat * 2.0, 675) #USD
+        else:
+            voucherCost = min(revenuePerSeat * 2.0, 1350) #USD
 
     revenue = np.zeros((nbFlights, maxOverbooking))
 
@@ -42,17 +55,17 @@ def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0,
         for f in range(0, nbFlights): # simulate nbFlights flights
             revenue[f][tix_overbooked] = simulate_net_revenue(nb_tickets_sold,
                                                               nbSeats,
-                                                              probabilityShow,
+                                                              probabilityShowDomestic, probabilityShowInter,
                                                               revenuePerSeat,
-                                                              voucher_cost)
+                                                              voucherCost)
 
     # print(revenue)
 
-    # f, ax = plt.subplots()
-    # l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    # ax.boxplot(revenue)
-    # ax.set_xticklabels(l)
-    # plt.show()
+    f, ax = plt.subplots()
+    l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    ax.boxplot(revenue)
+    ax.set_xticklabels(l)
+    plt.show()
 
 
     col = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
