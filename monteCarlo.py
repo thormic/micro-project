2251a427227464de08799417b1f348a17989ee66
Bartek <kowalczuk.bartek@gmail.com>
2019-02-09 16:53:58 +0100

    Style fix

--- monteCarlo.py
+++ monteCarlo.py
@@ -4,50 +4,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0,
-                nbFlights = 10000, maxOverbooking = 16):
+                nbFlights = 10000, maxOverbooking = 16, interDomestic = 0):
 
-    def show_up(probability_show):
-        if random.random() <= probability_show:
+    def show_up(probabilityShow):
+        if random.random() <= probabilityShow:
             return True
         else:
             return False
 
-    def simulate_flight(nb_tickets_sold, probability_show):
-        n = 0 # number of folks who bought tix that will show up
+    def simulate_flight(nb_tickets_sold, probabilityShow):
+        n = 0 # number of people who bought tix that will show up
         for i in range(0,nb_tickets_sold):
-            if show_up(probability_show):
+            if show_up(probabilityShow):
                 n = n + 1
         return n
 
-    def simulate_net_revenue(nb_tickets_sold, nb_seats,
-                             probability_show, revenue_per_seat, voucher_cost):
+    def simulate_net_revenue(nb_tickets_sold, nbSeats,
+                             probabilityShow, revenuePerSeat, voucher_cost):
         # how many ticket purchasers actually showed up?
-        nb_shows = simulate_flight(nb_tickets_sold, probability_show)
-        # no one bumped from flight if less or equal folks show up than for
+        nb_shows = simulate_flight(nb_tickets_sold, probabilityShow)
+        # no one bumped from flight if less or equal people show up than for
         #   the number of seats we have
-        if nb_shows <= nb_seats:
-            return revenue_per_seat * nb_shows
-        # if more customers show up than seats we hv, must pay out vouchers
+        if nb_shows <= nbSeats:
+            return revenuePerSeat * nb_shows
+        # if more customers show up than seats we have, must pay out vouchers
         else:
-            vouchers_out = nb_shows - nb_seats
-            return nb_seats * revenue_per_seat - voucher_cost * vouchers_out
+            vouchers_out = nb_shows - nbSeats
+            return nbSeats * revenuePerSeat - voucher_cost * vouchers_out
 
-    probability_show = probabilityShow
-    nb_seats = nbSeats
-    revenue_per_seat = revenuePerSeat # USD
-    voucher_cost = revenue_per_seat * 2.0 # USD
-    nb_flights = nbFlights
-    max_overbooking = maxOverbooking
 
-    revenue = np.zeros((nb_flights, max_overbooking))
+    voucher_cost = revenuePerSeat * 2.0 # USD
 
-    for tix_overbooked in range(0, max_overbooking):
-        nb_tickets_sold = nb_seats + tix_overbooked
-        for f in range(0, nb_flights): # simulate nb_flights flights
+    revenue = np.zeros((nbFlights, maxOverbooking))
+
+    for tix_overbooked in range(0, maxOverbooking):
+        nb_tickets_sold = nbSeats + tix_overbooked
+        for f in range(0, nbFlights): # simulate nbFlights flights
             revenue[f][tix_overbooked] = simulate_net_revenue(nb_tickets_sold,
-                                                              nb_seats,
-                                                              probability_show,
-                                                              revenue_per_seat,
+                                                              nbSeats,
+                                                              probabilityShow,
+                                                              revenuePerSeat,
                                                               voucher_cost)
 
     # print(revenue)
@@ -57,10 +53,6 @@ def monteCarlo (probabilityShow = 0.935, nbSeats = 100, revenuePerSeat = 450.0,
     # ax.boxplot(revenue)
     # ax.set_xticklabels(l)
     # plt.show()
-
-
-# for i in range(350, 500):
-#     monteCarlo(revenuePerSeat = i)
 
 
     col = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
